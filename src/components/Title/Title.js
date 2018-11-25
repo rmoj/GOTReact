8d6a0cd7c05150524ec3e6f85a05f313d95fd123
rmoj <rmoj99@gmail.com>
2018-11-25 00:59:05 -0500

    Fix(pix.json): relative paths not showing images when deployed to github

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -3,7 +3,7 @@ import './Title.css';
 
 const Title = props => (
   <div class="jumbotron text-center">
-    <h1 class="display-6">GOT React Clicky Game!</h1>
+    <h1 class="display-6">GOT React Memory Game</h1>
     <h3>
       Click on an image to earn points, but don't click on any more than once!
     </h3>
