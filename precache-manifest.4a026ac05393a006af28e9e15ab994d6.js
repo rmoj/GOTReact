524d3b61efa3036c92626d8d30b2b60e960621de
rmoj <rmoj99@gmail.com>
2018-11-25 00:20:31 -0500

    Updates

---
+++ precache-manifest.4a026ac05393a006af28e9e15ab994d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6427d16ceeaffc07efc1",
+    "url": "/GOTReact/static/js/runtime~main.6427d16c.js"
+  },
+  {
+    "revision": "3462ad03270fae6f2cb0",
+    "url": "/GOTReact/static/js/main.3462ad03.chunk.js"
+  },
+  {
+    "revision": "a1657e7cd1dc52ab5280",
+    "url": "/GOTReact/static/js/1.a1657e7c.chunk.js"
+  },
+  {
+    "revision": "3462ad03270fae6f2cb0",
+    "url": "/GOTReact/static/css/main.8755abc8.chunk.css"
+  },
+  {
+    "revision": "d65f9c54555075bb548c88abdea2ca36",
+    "url": "/GOTReact/index.html"
+  }
+];
