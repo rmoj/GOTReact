5cba60995f65a00a96b02770f29807f0597250e6
rmoj <rmoj99@gmail.com>
2018-11-24 23:50:41 -0500

    Create(): Title component

---
+++ src/components/Title/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Title.css';
+
+const Title = props => (
+  <div class="jumbotron text-center">
+    <h1 class="display-6">GOT React Clicky Game!</h1>
+    <h3>
+      Click on an image to earn points, but don't click on any more than once!
+    </h3>
+  </div>
+);
+
+export default Title;
